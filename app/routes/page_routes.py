ee4440659093da7794d412f976f186e29cd183bd
Ipseeta Priyadarshini <ipseeta.pkar@gmail.com>
2024-11-19 18:50:31 +0530

    added favicon

--- app/routes/page_routes.py
+++ app/routes/page_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, send_from_directory, current_app
+import os
 
 page_bp = Blueprint('page', __name__)
 
@@ -6,3 +7,19 @@ page_bp = Blueprint('page', __name__)
 @page_bp.route('/')
 def index():
     return render_template('index.html')
+
+@page_bp.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static/favicon'),
+        'favicon.ico', 
+        mimetype='image/x-icon'
+    )
+
+@page_bp.route('/site.webmanifest')
+def webmanifest():
+    return send_from_directory(
+        os.path.join(current_app.root_path, 'static/favicon'),
+        'site.webmanifest',
+        mimetype='application/manifest+json'
+    )
